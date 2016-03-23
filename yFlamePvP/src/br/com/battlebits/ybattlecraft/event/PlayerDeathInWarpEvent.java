c1b1a82db331153dfe3ee718c819dd3813aa9b3d
zViitoor <rodrigues.victor@outlook.com>
2016-03-23 20:42:54 -0300

    PlayerDeath corrigido

--- yFlamePvP/src/br/com/battlebits/ybattlecraft/event/PlayerDeathInWarpEvent.java
+++ yFlamePvP/src/br/com/battlebits/ybattlecraft/event/PlayerDeathInWarpEvent.java
@@ -2,62 +2,47 @@ package br.com.battlebits.ybattlecraft.event;
 
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
 
-public class PlayerDeathInWarpEvent extends Event {
+public class PlayerDeathInWarpEvent extends PlayerEvent {
 
 	public static HandlerList handlers = new HandlerList();
-	private UUID playerUUID;
-	private UUID killerUUID;
+	private Player killer;
 	private String kitName;
-	private boolean canceled;
 	private String warp;
 
 	public PlayerDeathInWarpEvent(Player p, Player killer, String warpName) {
-		this.playerUUID = p.getUniqueId();
+		super(p);
 		if (killer != null) {
-			this.killerUUID = killer.getUniqueId();
+			this.killer = killer;
 		}
 		this.warp = warpName;
 	}
 
-	public Player getPlayer() {
-		return Bukkit.getPlayer(playerUUID);
-	}
-
-	public UUID getPlayerUUID() {
-		return playerUUID;
-	}
-	
 	public String getWarp() {
 		return warp;
 	}
 
-	public UUID getKillerUUID() {
-		return killerUUID;
+	public UUID getPlayerUUID() {
+		return player.getUniqueId();
 	}
 
 	public Player getKiller() {
-		return Bukkit.getPlayer(killerUUID);
+		return killer;
+	}
+
+	public UUID getKillerUUID() {
+		return killer.getUniqueId();
 	}
 
 	public boolean hasKiller() {
-		return killerUUID != null;
+		return killer != null;
 	}
 
 	public String getKitName() {
 		return kitName;
-	}
-
-	public boolean isCanceled() {
-		return canceled;
-	}
-
-	public void setCanceled(boolean canceled) {
-		this.canceled = canceled;
 	}
 
 	public HandlerList getHandlers() {
