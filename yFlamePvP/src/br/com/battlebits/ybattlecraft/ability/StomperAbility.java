adda0529f037ab4de2f634c07a7d884e717ecf50
zViitoor <rodrigues.victor@outlook.com>
2016-03-25 17:10:18 -0300

    Corrigida a habilidade do Stomper

--- yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/StomperAbility.java
+++ yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/StomperAbility.java
@@ -1,5 +1,8 @@
 package br.com.battlebits.ybattlecraft.ability;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,6 +11,7 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import br.com.battlebits.ybattlecraft.yBattleCraft;
 import br.com.battlebits.ybattlecraft.base.BaseAbility;
+import br.com.battlebits.ybattlecraft.event.PlayerDeathInWarpEvent;
 
 public class StomperAbility extends BaseAbility {
 
@@ -16,12 +20,13 @@ public class StomperAbility extends BaseAbility {
 	}
 
 	@SuppressWarnings("deprecation")
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onEntityDamageListener(EntityDamageEvent e) {
 		if (e.getCause() == DamageCause.FALL) {
 			if (e.getEntity() instanceof Player) {
 				Player p = (Player) e.getEntity();
 				if (isUsing(p)) {
+					p.getWorld().playSound(p.getLocation(), Sound.ANVIL_LAND, 0.15F, 1.0F);
 					for (Player ps : p.getWorld().getEntitiesByClass(p.getClass())) {
 						if (ps.getUniqueId() != p.getUniqueId()) {
 							if (!battlecraft.getProtectionManager().isProtected(ps.getUniqueId())) {
@@ -31,7 +36,12 @@ public class StomperAbility extends BaseAbility {
 										if (ps.isSneaking() && dmg > 8) {
 											dmg = 8;
 										}
-										ps.damage(dmg, p);
+										if (dmg >= ((Damageable) ps).getHealth()) {
+											Bukkit.getPluginManager().callEvent(
+													new PlayerDeathInWarpEvent(ps, p, battlecraft.getWarpManager().getPlayerWarp(ps.getUniqueId())));
+										} else {
+											ps.damage(dmg, p);
+										}
 									}
 								}
 							}
