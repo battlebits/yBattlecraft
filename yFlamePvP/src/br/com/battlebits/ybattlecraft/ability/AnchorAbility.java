e88b8ebf56dc0cfb98c98b23a1acb22a0365204b
zViitoor <rodrigues.victor@outlook.com>
2016-03-25 17:11:49 -0300

    Habilidade Anchor corrigida

--- yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/AnchorAbility.java
+++ yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/AnchorAbility.java
@@ -1,14 +1,17 @@
 package br.com.battlebits.ybattlecraft.ability;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import br.com.battlebits.ybattlecraft.yBattleCraft;
 import br.com.battlebits.ybattlecraft.base.BaseAbility;
+import br.com.battlebits.ybattlecraft.event.PlayerDeathInWarpEvent;
 
 public class AnchorAbility extends BaseAbility {
 
@@ -16,19 +19,44 @@ public class AnchorAbility extends BaseAbility {
 		super(yBattleCraft);
 	}
 
+	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.LOW)
 	public void onPlayerDamagePlayerListener(EntityDamageByEntityEvent e) {
-		if (e.getEntity() instanceof Player && e.getDamager() instanceof Player) {
+		if (e.getEntity() instanceof Player) {
 			Player p = (Player) e.getEntity();
-			Player d = (Player) e.getDamager();
-			if ((!battlecraft.getProtectionManager().isProtected(p.getUniqueId()))
-					&& (!battlecraft.getProtectionManager().isProtected(d.getUniqueId()))) {
-				if (isUsing(p) || isUsing(d)) {
-					p.getWorld().playSound(p.getLocation(), Sound.ANVIL_LAND, 0.15F, 1.0F);
-					if (e.getFinalDamage() < ((Damageable) p).getHealth()) {
-						p.damage(e.getFinalDamage());
-						p.setLastDamageCause(e);
-						e.setCancelled(true);
+			if (e.getDamager() instanceof Player) {
+				Player d = (Player) e.getDamager();
+				if ((!battlecraft.getProtectionManager().isProtected(p.getUniqueId()))
+						&& (!battlecraft.getProtectionManager().isProtected(d.getUniqueId()))) {
+					if (isUsing(p) || isUsing(d)) {
+						p.getWorld().playSound(p.getLocation(), Sound.ANVIL_LAND, 0.15F, 1.0F);
+						if (e.getDamage() >= ((Damageable) p).getHealth()) {
+							Bukkit.getPluginManager()
+									.callEvent(new PlayerDeathInWarpEvent(p, d, battlecraft.getWarpManager().getPlayerWarp(p.getUniqueId())));
+						} else {
+							p.damage(e.getFinalDamage());
+							p.setLastDamageCause(e);
+							e.setCancelled(true);
+						}
+					}
+				}
+			} else if (e.getDamager() instanceof Projectile) {
+				Projectile pro = (Projectile) e.getDamager();
+				if(pro.getShooter() != null && pro.getShooter() instanceof Player){
+					Player d = (Player) pro.getShooter();
+					if ((!battlecraft.getProtectionManager().isProtected(p.getUniqueId()))
+							&& (!battlecraft.getProtectionManager().isProtected(d.getUniqueId()))) {
+						if (isUsing(p) || isUsing(d)) {
+							p.getWorld().playSound(p.getLocation(), Sound.ANVIL_LAND, 0.15F, 1.0F);
+							if (e.getDamage() >= ((Damageable) p).getHealth()) {
+								Bukkit.getPluginManager()
+										.callEvent(new PlayerDeathInWarpEvent(p, d, battlecraft.getWarpManager().getPlayerWarp(p.getUniqueId())));
+							} else {
+								p.damage(e.getFinalDamage());
+								p.setLastDamageCause(e);
+								e.setCancelled(true);
+							}
+						}
 					}
 				}
 			}
