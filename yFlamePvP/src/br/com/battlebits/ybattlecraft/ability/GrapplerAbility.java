cd71d1886993b5afa0ce706a46299a6e3a7cff85
flametuner <flame.tuner@gmail.com>
2016-05-30 14:43:02 -0300

    Removido cooldown de Grappler e Kangaroo

--- yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/GrapplerAbility.java
+++ yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/GrapplerAbility.java
@@ -22,7 +22,6 @@ import br.com.battlebits.ybattlecraft.yBattleCraft;
 import br.com.battlebits.ybattlecraft.base.BaseAbility;
 import br.com.battlebits.ybattlecraft.builder.ItemBuilder;
 import br.com.battlebits.ybattlecraft.constructors.GrapplingHook;
-import br.com.battlebits.ybattlecraft.event.PlayerDamagePlayerEvent;
 import br.com.battlebits.ybattlecraft.event.PlayerDeathInWarpEvent;
 
 public class GrapplerAbility extends BaseAbility {
@@ -45,30 +44,23 @@ public class GrapplerAbility extends BaseAbility {
 					e.setCancelled(true);
 					if (grapplerHooks.containsKey(e.getPlayer().getUniqueId())) {
 						if (grapplerHooks.get(e.getPlayer().getUniqueId()).isHooked()) {
-							if (!battlecraft.getCooldownManager().isOnCooldown(e.getPlayer().getUniqueId(), "grapplerability")) {
-								GrapplingHook hook = grapplerHooks.get(e.getPlayer().getUniqueId());
-								Location loc = hook.getBukkitEntity().getLocation();
-								Location playerLoc = e.getPlayer().getLocation();
-								double d = loc.distance(playerLoc);
-								double t = d;
-								double v_x = (1.0D + 0.04000000000000001D * t) * ((isNear(loc, playerLoc) ? 0 : loc.getX() - playerLoc.getX()) / t);
-								double v_y = (0.9D + 0.03D * t) * ((isNear(loc, playerLoc) ? 0.1 : loc.getY() - playerLoc.getY()) / t);
-								double v_z = (1.0D + 0.04000000000000001D * t) * ((isNear(loc, playerLoc) ? 0 : loc.getZ() - playerLoc.getZ()) / t);
-								Vector v = e.getPlayer().getVelocity();
-								v.setX(v_x);
-								v.setY(v_y);
-								v.setZ(v_z);
-								e.getPlayer().setVelocity(v);
-								if (playerLoc.getY() < hook.getBukkitEntity().getLocation().getY()) {
-									e.getPlayer().setFallDistance(0);
-								}
-								e.getPlayer().getWorld().playSound(playerLoc, Sound.STEP_GRAVEL, 1.0F, 1.0F);
-							} else {
-								e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.IRONGOLEM_HIT, 0.5F, 1.0F);
-								e.getPlayer().sendMessage("§5§LGRAPPLER §fAguarde §9§l"
-										+ battlecraft.getCooldownManager().getCooldownTimeFormated(e.getPlayer().getUniqueId(), "grapplerability").toUpperCase()
-										+ "§f para utilizar sua habilidade!");
+							GrapplingHook hook = grapplerHooks.get(e.getPlayer().getUniqueId());
+							Location loc = hook.getBukkitEntity().getLocation();
+							Location playerLoc = e.getPlayer().getLocation();
+							double d = loc.distance(playerLoc);
+							double t = d;
+							double v_x = (1.0D + 0.04000000000000001D * t) * ((isNear(loc, playerLoc) ? 0 : loc.getX() - playerLoc.getX()) / t);
+							double v_y = (0.9D + 0.03D * t) * ((isNear(loc, playerLoc) ? 0.1 : loc.getY() - playerLoc.getY()) / t);
+							double v_z = (1.0D + 0.04000000000000001D * t) * ((isNear(loc, playerLoc) ? 0 : loc.getZ() - playerLoc.getZ()) / t);
+							Vector v = e.getPlayer().getVelocity();
+							v.setX(v_x);
+							v.setY(v_y);
+							v.setZ(v_z);
+							e.getPlayer().setVelocity(v);
+							if (playerLoc.getY() < hook.getBukkitEntity().getLocation().getY()) {
+								e.getPlayer().setFallDistance(0);
 							}
+							e.getPlayer().getWorld().playSound(playerLoc, Sound.STEP_GRAVEL, 1.0F, 1.0F);
 						}
 					}
 				}
@@ -100,45 +92,30 @@ public class GrapplerAbility extends BaseAbility {
 							grapplerHooks.remove(e.getPlayer().getUniqueId());
 						}
 						GrapplingHook hook = new GrapplingHook(e.getPlayer().getWorld(), ((CraftPlayer) e.getPlayer()).getHandle());
-						hook.spawn(e.getPlayer().getEyeLocation().add(e.getPlayer().getLocation().getDirection().getX(),
-								e.getPlayer().getLocation().getDirection().getY(), e.getPlayer().getLocation().getDirection().getZ()));
-						hook.move(e.getPlayer().getLocation().getDirection().getX() * 5.0D, e.getPlayer().getLocation().getDirection().getY() * 5.0D,
-								e.getPlayer().getLocation().getDirection().getZ() * 5.0D);
+						hook.spawn(e.getPlayer().getEyeLocation().add(e.getPlayer().getLocation().getDirection().getX(), e.getPlayer().getLocation().getDirection().getY(), e.getPlayer().getLocation().getDirection().getZ()));
+						hook.move(e.getPlayer().getLocation().getDirection().getX() * 5.0D, e.getPlayer().getLocation().getDirection().getY() * 5.0D, e.getPlayer().getLocation().getDirection().getZ() * 5.0D);
 						grapplerHooks.put(e.getPlayer().getUniqueId(), hook);
 					} else if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
 						if (grapplerHooks.containsKey(e.getPlayer().getUniqueId())) {
-							if (!battlecraft.getCooldownManager().isOnCooldown(e.getPlayer().getUniqueId(), "grapplerability")) {
-								if (grapplerHooks.get(e.getPlayer().getUniqueId()).isHooked()) {
-									GrapplingHook hook = grapplerHooks.get(e.getPlayer().getUniqueId());
-									Location loc = hook.getBukkitEntity().getLocation();
-									double d = loc.distance(e.getPlayer().getLocation());
-									double t = d;
-									double v_x = (1.0D + 0.04000000000000001D * t)
-											* ((isNear(loc, e.getPlayer().getLocation()) ? 0 : loc.getX() - e.getPlayer().getLocation().getX()) / t);
-									double v_y = (0.9D + 0.03D * t)
-											* ((isNear(loc, e.getPlayer().getLocation()) ? 0.1 : loc.getY() - e.getPlayer().getLocation().getY())
-													/ t);
-									double v_z = (1.0D + 0.04000000000000001D * t)
-											* ((isNear(loc, e.getPlayer().getLocation()) ? 0 : loc.getZ() - e.getPlayer().getLocation().getZ()) / t);
-									Vector v = e.getPlayer().getVelocity();
-									v.setX(v_x);
-									v.setY(v_y);
-									v.setZ(v_z);
-									e.getPlayer().setVelocity(v);
-									if (e.getPlayer().getLocation().getY() < hook.getBukkitEntity().getLocation().getY()) {
-										e.getPlayer().setFallDistance(0);
-									}
-									e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(), Sound.STEP_GRAVEL, 1.0F, 1.0F);
-								} else {
-									e.getPlayer().sendMessage("§5§lGRAPPLER §fSua corda ainda não foi §9§lFISGADA§f!");
+							if (grapplerHooks.get(e.getPlayer().getUniqueId()).isHooked()) {
+								GrapplingHook hook = grapplerHooks.get(e.getPlayer().getUniqueId());
+								Location loc = hook.getBukkitEntity().getLocation();
+								double d = loc.distance(e.getPlayer().getLocation());
+								double t = d;
+								double v_x = (1.0D + 0.04000000000000001D * t) * ((isNear(loc, e.getPlayer().getLocation()) ? 0 : loc.getX() - e.getPlayer().getLocation().getX()) / t);
+								double v_y = (0.9D + 0.03D * t) * ((isNear(loc, e.getPlayer().getLocation()) ? 0.1 : loc.getY() - e.getPlayer().getLocation().getY()) / t);
+								double v_z = (1.0D + 0.04000000000000001D * t) * ((isNear(loc, e.getPlayer().getLocation()) ? 0 : loc.getZ() - e.getPlayer().getLocation().getZ()) / t);
+								Vector v = e.getPlayer().getVelocity();
+								v.setX(v_x);
+								v.setY(v_y);
+								v.setZ(v_z);
+								e.getPlayer().setVelocity(v);
+								if (e.getPlayer().getLocation().getY() < hook.getBukkitEntity().getLocation().getY()) {
+									e.getPlayer().setFallDistance(0);
 								}
+								e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(), Sound.STEP_GRAVEL, 1.0F, 1.0F);
 							} else {
-								if (grapplerHooks.get(e.getPlayer().getUniqueId()).hooked == null) {
-									e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.IRONGOLEM_HIT, 0.5F, 1.0F);
-									e.getPlayer().sendMessage("§5§LGRAPPLER §fAguarde §9§l"
-											+ battlecraft.getCooldownManager().getCooldownTimeFormated(e.getPlayer().getUniqueId(), "grapplerability").toUpperCase()
-											+ "§f para utilizar sua habilidade!");
-								}
+								e.getPlayer().sendMessage("§5§lGRAPPLER §fSua corda ainda não foi §9§lFISGADA§f!");
 							}
 						}
 					}
@@ -152,27 +129,6 @@ public class GrapplerAbility extends BaseAbility {
 		if (grapplerHooks.containsKey(e.getPlayerUUID())) {
 			grapplerHooks.get(e.getPlayerUUID()).remove();
 			grapplerHooks.remove(e.getPlayerUUID());
-		}
-		if (e.hasKiller()) {
-			if (isUsing(e.getKiller())) {
-				battlecraft.getCooldownManager().removeCooldown(e.getKillerUUID(), "grapplerability");
-			}
-		}
-	}
-
-	@EventHandler
-	public void onPlayerDamagePlayerListener(PlayerDamagePlayerEvent e) {
-		if (isUsing(e.getDamager())) {
-			if (!battlecraft.getCooldownManager().isOnCooldown(e.getDamager().getUniqueId(), "grapplerability")) {
-				e.getDamager().sendMessage("§5§lGRAPPLER §fVoce entrou em cooldown por entrar em §9§LCOMBATE§F!");
-			}
-			battlecraft.getCooldownManager().setCooldown(e.getDamager().getUniqueId(), "grapplerability", 5);
-		}
-		if (isUsing(e.getDamaged())) {
-			if (!battlecraft.getCooldownManager().isOnCooldown(e.getDamaged().getUniqueId(), "grapplerability")) {
-				e.getDamaged().sendMessage("§5§lGRAPPLER §fVoce entrou em cooldown por entrar em §9§LCOMBATE§F!");
-			}
-			battlecraft.getCooldownManager().setCooldown(e.getDamaged().getUniqueId(), "grapplerability", 5);
 		}
 	}
 
--- yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/KangarooAbility.java
+++ yFlamePvP/src/br/com/battlebits/ybattlecraft/ability/KangarooAbility.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,8 +20,6 @@ import org.bukkit.util.Vector;
 import br.com.battlebits.ybattlecraft.yBattleCraft;
 import br.com.battlebits.ybattlecraft.base.BaseAbility;
 import br.com.battlebits.ybattlecraft.builder.ItemBuilder;
-import br.com.battlebits.ybattlecraft.event.PlayerDamagePlayerEvent;
-import br.com.battlebits.ybattlecraft.event.PlayerDeathInWarpEvent;
 
 public class KangarooAbility extends BaseAbility {
 
@@ -32,8 +29,7 @@ public class KangarooAbility extends BaseAbility {
 	public KangarooAbility(yBattleCraft yBattleCraft) {
 		super(yBattleCraft);
 		doubleJump = new ArrayList<>();
-		abilityItem = new ItemBuilder().amount(1).type(Material.FIREWORK).glow().lore("§7Utilize seu foguete para ser lancado para onde voce desejar")
-				.name("§6§lKangaroo Boost").build();
+		abilityItem = new ItemBuilder().amount(1).type(Material.FIREWORK).glow().lore("§7Utilize seu foguete para ser lancado para onde voce desejar").name("§6§lKangaroo Boost").build();
 		getItens().add(abilityItem);
 		new BukkitRunnable() {
 			@SuppressWarnings({ "deprecation", "unchecked" })
@@ -58,41 +54,34 @@ public class KangarooAbility extends BaseAbility {
 			if (e.getPlayer().getItemInHand() != null) {
 				if (e.getPlayer().getItemInHand().equals(abilityItem)) {
 					if (e.getAction() != Action.PHYSICAL) {
-						if (!battlecraft.getCooldownManager().isOnCooldown(e.getPlayer().getUniqueId(), "kangarooability")) {
-							if (e.getPlayer().isOnGround()) {
+						if (e.getPlayer().isOnGround()) {
+							Vector vector = e.getPlayer().getEyeLocation().getDirection();
+							if (!e.getPlayer().isSneaking()) {
+								vector.multiply(0.3F);
+								vector.setY(0.9F);
+							} else {
+								vector.multiply(0.3F);
+								vector.setY(0.65F);
+							}
+							e.getPlayer().setVelocity(vector);
+							if (doubleJump.contains(e.getPlayer().getUniqueId())) {
+								doubleJump.remove(e.getPlayer().getUniqueId());
+							}
+						} else {
+							if (!doubleJump.contains(e.getPlayer().getUniqueId())) {
 								Vector vector = e.getPlayer().getEyeLocation().getDirection();
 								if (!e.getPlayer().isSneaking()) {
 									vector.multiply(0.3F);
-									vector.setY(0.9F);
+									vector.setY(0.85F);
+									e.getPlayer().setVelocity(vector);
+									doubleJump.add(e.getPlayer().getUniqueId());
 								} else {
-									vector.multiply(0.3F);
+									vector.multiply(1F);
 									vector.setY(0.65F);
-								}
-								e.getPlayer().setVelocity(vector);
-								if (doubleJump.contains(e.getPlayer().getUniqueId())) {
-									doubleJump.remove(e.getPlayer().getUniqueId());
-								}
-							} else {
-								if (!doubleJump.contains(e.getPlayer().getUniqueId())) {
-									Vector vector = e.getPlayer().getEyeLocation().getDirection();
-									if (!e.getPlayer().isSneaking()) {
-										vector.multiply(0.3F);
-										vector.setY(0.85F);
-										e.getPlayer().setVelocity(vector);
-										doubleJump.add(e.getPlayer().getUniqueId());
-									} else {
-										vector.multiply(1F);
-										vector.setY(0.65F);
-										e.getPlayer().setVelocity(vector);
-										doubleJump.add(e.getPlayer().getUniqueId());
-									}
+									e.getPlayer().setVelocity(vector);
+									doubleJump.add(e.getPlayer().getUniqueId());
 								}
 							}
-						} else {
-							e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.IRONGOLEM_HIT, 0.5F, 1.0F);
-							e.getPlayer().sendMessage("§5§lKANGAROO §fAguarde §9§l"
-									+ battlecraft.getCooldownManager().getCooldownTimeFormated(e.getPlayer().getUniqueId(), "kangarooability").toUpperCase()
-									+ "§f para utilizar sua habilidade!");
 						}
 						e.setCancelled(true);
 					}
@@ -118,31 +107,6 @@ public class KangarooAbility extends BaseAbility {
 						e.setDamage(7.0);
 					}
 				}
-			}
-		}
-	}
-
-	@EventHandler
-	public void onPlayerDamagePlayerListener(PlayerDamagePlayerEvent e) {
-		if (isUsing(e.getDamager())) {
-			if (!battlecraft.getCooldownManager().isOnCooldown(e.getDamager().getUniqueId(), "kangarooability")) {
-				e.getDamager().sendMessage("§5§LKANGAROO §fVoce entrou em cooldown por entrar em §9§LCOMBATE§F!");
-			}
-			battlecraft.getCooldownManager().setCooldown(e.getDamager().getUniqueId(), "kangarooability", 5);
-		}
-		if (isUsing(e.getDamaged())) {
-			if (!battlecraft.getCooldownManager().isOnCooldown(e.getDamaged().getUniqueId(), "kangarooability")) {
-				e.getDamaged().sendMessage("§5§LKANGAROO §fVoce entrou em cooldown por entrar em §9§LCOMBATE§F!");
-			}
-			battlecraft.getCooldownManager().setCooldown(e.getDamaged().getUniqueId(), "kangarooability", 5);
-		}
-	}
-
-	@EventHandler
-	public void onPlayerDeathInWarpListener(PlayerDeathInWarpEvent e) {
-		if (e.hasKiller()) {
-			if (isUsing(e.getKiller())) {
-				battlecraft.getCooldownManager().removeCooldown(e.getKillerUUID(), "kangarooability");
 			}
 		}
 	}
